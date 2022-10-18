714c08507272726fd0ccab2c0105a8cf5fb64ee5
DavidFFDAW <davidferflo2@gmail.com>
2022-10-19 01:53:35 +0200

    feat: user model, config and css

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,4 @@
 export class Config {
-    public static readonly API_URL = 'http://localhost:3000';
+    // public static readonly API_URL = 'http://localhost:3000';
+    public static readonly API_BASE_URL = 'http://vps-f87b433e.vps.ovh.net/2k/api/v2/';
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,13 @@
 export interface User {
+    id?: number;
     name: string;
-    email?: string;
+    email: string;
+    username?: string;
+    token?: string;
+}
+
+export interface PersistableUser {
+    id?: number;
+    username?: string;
     token?: string;
 }
