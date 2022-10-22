dffc148a3c62c0d02f8d9459d6ef9d258a3274a6
DavidFFDAW <davidferflo2@gmail.com>
2022-10-22 18:59:25 +0200

    refactor: header button to log user out with redirection

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
 import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
-import { getPersistedUserObject } from '../../services/auth.service';
+import { getPersistedUserObject, removePersistedUserObject } from '../../services/auth.service';
 import { PersistableUser } from '../../models/User';
 import { Link, useNavigate } from 'react-router-dom';
-import { PrivateRoutes } from '../../models/routes';
+import { AppRoutes, PrivateRoutes } from '../../models/routes';
 
 export default function Header() {
-    const loggedUser: PersistableUser = getPersistedUserObject();
+    // const loggedUser: PersistableUser = getPersistedUserObject();
     const navigate = useNavigate();
+
+    const logout = () => {
+        removePersistedUserObject();
+        navigate(`/${AppRoutes.LOGIN}`, { replace: true });
+    };
 
     const goBack = () => {
         navigate(-1);
@@ -32,9 +36,14 @@ export default function Header() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         2k Manager
                     </Typography>
-                    <Link to={PrivateRoutes.USER} className="link">
+
+                    {/* <Link to={PrivateRoutes.USER} className="link">
                         <Button color="inherit">{loggedUser.username}</Button>
-                    </Link>
+                    </Link> */}
+
+                    <Button color="inherit" onClick={logout}>
+                        Logout
+                    </Button>
                 </Toolbar>
             </AppBar>
         </Box>
