bb2887cd1114bf9a0d93afb5d5efaa48ee09243d
DavidFFDAW <davidferflo2@gmail.com>
2022-10-24 02:21:17 +0200

    fix: type error any on context

--- src/contexts/user.context.tsx
+++ src/contexts/user.context.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
+import { PersistableUser } from '../models/User';
 import { getPersistedUserObject } from '../services/auth.service';
 
 export interface CType {
     storedUser?: any;
-    setStoredUser?: (user: any) => void;
+    setStoredUser?: any;
 }
 
 const UserContext: React.Context<CType> = React.createContext({});
 
 export const UserProvider = ({ children }: any) => {
-    const [storedUser, setStoredUser] = useState(() => getPersistedUserObject());
+    const [storedUser, setStoredUser] = useState<PersistableUser>(() => getPersistedUserObject());
 
     return <UserContext.Provider value={{ storedUser, setStoredUser }}>{children}</UserContext.Provider>;
 };
