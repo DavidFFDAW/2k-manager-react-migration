12d524e01019ce4d5e947816f0571f150a8dceb5
DavidFFDAW <davidferflo2@gmail.com>
2022-10-24 02:10:40 +0200

    feat: theme and ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,46 @@ import { CircularProgress } from '@mui/material';
 import { SnackbarProvider } from 'notistack';
 import { Suspense, lazy } from 'react';
 import { BrowserRouter, Navigate, Route } from 'react-router-dom';
-import Links from './components/Links';
+import { ThemeProvider } from '@emotion/react';
 import RoutesWithNotFound from './components/RoutesWithNotFound';
 import { UserProvider } from './contexts/user.context';
 import { AuthGuard } from './guards/auth.guard';
 import { AppRoutes, PrivateRoutes } from './models/routes';
-import './App.css';
 import { LoginAuthGuard } from './guards/login.guard';
+import theme from './theme';
+import './App.css';
 
 const Login = lazy(() => import('./pages/Login'));
 const PrivateRoutingModule = lazy(() => import('./pages/Private/PrivateRoutingModule'));
 
 function App() {
     return (
-        <SnackbarProvider maxSnack={4}>
-            <Suspense fallback={<CircularProgress />}>
-                <BrowserRouter>
-                    <UserProvider>
-                        {/* <Links /> */}
-                        <RoutesWithNotFound>
-                            <Route path="/" element={<Navigate to={AppRoutes.LOGIN} replace />} />
+        <ThemeProvider theme={theme}>
+            <SnackbarProvider
+                maxSnack={4}
+                autoHideDuration={3500}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <Suspense fallback={<CircularProgress />}>
+                    <BrowserRouter>
+                        <UserProvider>
+                            {/* <Links /> */}
+                            <RoutesWithNotFound>
+                                <Route path="/" element={<Navigate to={AppRoutes.LOGIN} replace />} />
 
-                            <Route element={<LoginAuthGuard />}>
-                                <Route path={AppRoutes.LOGIN} element={<Login />} />
-                            </Route>
+                                <Route element={<LoginAuthGuard />}>
+                                    <Route path={AppRoutes.LOGIN} element={<Login />} />
+                                </Route>
 
-                            <Route element={<AuthGuard privateValidation={true} />}>
-                                <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<PrivateRoutingModule />} />
-                            </Route>
-                        </RoutesWithNotFound>
-                    </UserProvider>
-                </BrowserRouter>
-            </Suspense>
-        </SnackbarProvider>
+                                <Route element={<AuthGuard privateValidation={true} />}>
+                                    <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<PrivateRoutingModule />} />
+                                </Route>
+                            </RoutesWithNotFound>
+                        </UserProvider>
+                    </BrowserRouter>
+                </Suspense>
+            </SnackbarProvider>
+        </ThemeProvider>
     );
 }
 
