50367277db5a687a458874228da24c9674a4d649
DavidFFDAW <davidferflo2@gmail.com>
2022-10-24 02:11:13 +0200

    refactor: new USER_KEY in AppConfig

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,4 +1,5 @@
-export class Config {
+export class AppConfig {
     // public static readonly API_URL = 'http://localhost:3000';
-    public static readonly API_BASE_URL = 'http://vps-f87b433e.vps.ovh.net/2k/api/v2/';
+    public static readonly API_BASE_URL = 'https://vps-f87b433e.vps.ovh.net/2k/api/v2/';
+    public static readonly USER_KEY = 'react-champions-user';
 }
