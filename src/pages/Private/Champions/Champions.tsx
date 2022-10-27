6411e704649904305ee69b5845dbadf015b983b2
DavidFFDAW <davidferflo2@gmail.com>
2022-10-28 01:03:48 +0200

    feat: champions page can be used public and admin

--- src/pages/Private/Champions/Champions.tsx
+++ src/pages/Private/Champions/Champions.tsx
@@ -1,12 +1,13 @@
 import { useFetchReigns } from './hooks/useFetchReigns';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import { BoxContainer } from '../../../components/DashboardLink/BoxContainer';
+import { BoxContainer } from '../../../components/BoxContainer/BoxContainer';
 import { ChampionsTable } from '../../../components/Table/ChampionsTable';
 import { TableSkeleton } from '../../../components/Table/TableSkeleton';
 
-export default function Champions() {
+export default function Champions({ hasAdminPermission }: any) {
     const { reigns, isFetching } = useFetchReigns();
+    const isAdmin = Boolean(hasAdminPermission) ? hasAdminPermission : false;
 
     if (isFetching) {
         return (
@@ -18,17 +19,17 @@ export default function Champions() {
 
     return (
         <>
-            <BoxContainer>
-                <h3>Singles</h3>
+            <BoxContainer title="Singles">
                 <ChampionsTable rows={reigns.currentSingles} />
             </BoxContainer>
 
-            <Fab color="primary" aria-label="add" className="fab">
-                <AddIcon />
-            </Fab>
+            {isAdmin && (
+                <Fab color="primary" aria-label="add" className="fab">
+                    <AddIcon />
+                </Fab>
+            )}
 
-            <BoxContainer>
-                <h3>Tag Teams</h3>
+            <BoxContainer title="Tag Teams">
                 <ChampionsTable rows={reigns.currentTagTeams} />
             </BoxContainer>
         </>
