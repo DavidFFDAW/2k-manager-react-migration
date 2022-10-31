d54b616f584dbc6f2d0262fff97dd19d2619fdb6
DavidFFDAW <davidferflo2@gmail.com>
2022-10-31 18:38:34 +0100

    feat: new data models

--- src/models/API.ts
+++ src/models/API.ts
@@ -27,6 +27,36 @@ export interface Champion {
     wrestlerImage: string;
     wrestlerName: string;
 }
+export interface Team {
+    brand: string;
+    championship: string;
+    championshipId: string;
+    championshipImage: string;
+    overall: string;
+    reignDays: string;
+    reignId: string;
+    totalDays: string;
+    totalReigns: string;
+    wrestlerId: string;
+    wrestlerImage: string;
+    wrestlerName: string;
+}
+export interface Report {
+    id: number;
+    image: string;
+    title: string;
+    content: string;
+    admin_id: number;
+    created_at: string;
+    updated_at: string;
+    exceptr: string;
+    visible: number | boolean;
+    category: string;
+}
+
+export interface NewsResponse {
+    news: Report[];
+}
 
 export interface ChampionsPartialResponse {
     currentSingles: Champion[];
