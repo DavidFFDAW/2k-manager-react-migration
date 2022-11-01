e3dbfd013e89ec499d479b991fc43a814953e339
DavidFFDAW <davidferflo2@gmail.com>
2022-11-02 00:48:29 +0100

    feat: push notifications key and App version

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,6 +1,11 @@
 export class AppConfig {
+    public static readonly VERSION = '1.1.0';
     // public static readonly API_URL = 'http://localhost:3000';
     public static readonly API_BASE_URL = 'https://vps-f87b433e.vps.ovh.net/2k/api/v2/';
     public static readonly USER_KEY = 'react-champions-user';
     public static readonly NEWS_ENDPOINT = AppConfig.API_BASE_URL + 'news/report';
+    public static readonly TEAMS_ENDPOINT = AppConfig.API_BASE_URL + 'teams/names';
+
+    public static readonly PUSH_VAPID_PUBLIC_KEY =
+        'BMOETqEVUA2CghOVQVUzTku_oebDMEs4uYokOESS3-eIfOX931bVPFBGo6m2oj-dFZga4tjz4nMKssadJiN8mL4';
 }
--- src/pages/Private/Dashboard/index.tsx
+++ src/pages/Private/Dashboard/index.tsx
@@ -6,11 +6,15 @@ import ArticleIcon from '@mui/icons-material/Article';
 import GroupsIcon from '@mui/icons-material/Groups';
 import { BoxContainer } from '../../../components/BoxContainer/BoxContainer';
 import { makePrivateRoute } from '../../../utilities/private.route.utility';
+import { Typography } from '@mui/material';
+import { AppConfig } from '../../../AppConfig';
 
 export default function Dashboard() {
     return (
         <div>
             <BoxContainer>
+                <Typography component="div">Version {AppConfig.VERSION}</Typography>
+
                 <DashboardLink link={makePrivateRoute(PrivateRoutes.USER)} title="User" icon={<PersonIcon />} />
                 <DashboardLink
                     link={makePrivateRoute(PrivateRoutes.CHAMPIONS)}
