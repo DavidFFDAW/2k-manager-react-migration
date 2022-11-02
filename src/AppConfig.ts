d23897c1cf8af06eadeb21d012e604d2808afe53
DavidFFDAW <davidferflo2@gmail.com>
2022-11-02 22:07:42 +0100

    feat: little changes to css and new version

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,5 +1,5 @@
 export class AppConfig {
-    public static readonly VERSION = '1.1.0';
+    public static readonly VERSION = '1.2.0';
     // public static readonly API_URL = 'http://localhost:3000';
     public static readonly API_BASE_URL = 'https://vps-f87b433e.vps.ovh.net/2k/api/v2/';
     public static readonly USER_KEY = 'react-champions-user';
