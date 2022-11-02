2b8288673f59df19698d0ba52c8f1ea36505f101
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-11-02 10:45:24 +0100

    refactor: service worker push notification registration

--- src/registerSW.ts
+++ src/registerSW.ts
@@ -1,17 +1,71 @@
 import { AppConfig } from './AppConfig';
 
+
+const urlB64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+}
+
+const showLocalNotification = (title, body, swRegistration) => {
+    const options = {
+      body,
+      // here you can add more properties like icon, image, vibrate, etc.
+    }
+    swRegistration.showNotification(title, options)
+}
+
+
+const getSubscriptionAndAddToken = (registration: any) => {
+    return registration.pushManager.getSubscription().then(subscription => {
+        console.log('Subscription: ', subscription);
+        if (!subscription) {
+            const generatedSubscription = registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlB64ToUint8Array(AppConfig.PUSH_VAPID_PUBLIC_KEY),
+            });
+            console.log(generatedSubscription);
+            
+            registration.addEventListener('push', function(event) {
+                if (event.data) {
+                  console.log('Push event!! ', event.data.text())
+                  showLocalNotification('Test Data', event.data.text(), registration)
+                } else {
+                  console.log('Push event but no data')
+                }
+            })
+
+            return generatedSubscription;            
+        }
+        return subscription;
+    });
+}
+
 export function register() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(registration => {
                 console.log('SW registered: ', registration);
-                return registration.pushManager.getSubscription().then(subscription => {
-                    console.log('Subscription: ', subscription);
-                    return registration.pushManager.subscribe({
-                        userVisibleOnly: true,
-                        applicationServerKey: AppConfig.PUSH_VAPID_PUBLIC_KEY,
+                if (!("Notification" in window)) return console.log('Push notifications are not allowed on this browser');
+                const { permission } = window.Notification;
+
+                if (permission === 'denied') return console.log('This user navigator has explicitly not allowed push notifications');
+                
+                if (permission === 'granted') return getSubscriptionAndAddToken(registration);
+
+                if (permission === 'default') {
+                    return window.Notification.requestPermission().then((permission: string) => {
+                        if ( permission === 'granted') {
+                            return getSubscriptionAndAddToken(registration);  
+                        }
                     });
-                });
+                }
+                
             });
         });
     }
